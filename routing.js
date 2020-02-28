4efc6d8627800e52415ff827fb36617eb8932da6
Wwashy <washingndi@gmail.com>
2020-02-28 18:31:12 +0300

    altered the routing final

--- routing.js
+++ routing.js
@@ -21,7 +21,7 @@ route.get('/patient', function (req, res) {
     res.sendFile(__dirname + '/patient.html');
 });
 route.get('/service', function (req, res) {
-    res.sendFile(__dirname + '/contact.html');
+    res.sendFile(__dirname + '/service.html');
 });
 route.get('/search', function (req, res) {
     res.sendFile(__dirname + '/search.html');
@@ -32,5 +32,68 @@ route.get('/search', function (req, res) {
 route.get('/schedules', function (req, res) {
     res.sendFile(__dirname + '/schedules.html');
 });
+
+
+//creates connection to the database
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'clinicsystem'
+});
+connection.connect(function (err) {
+    if (err) {throw err;
+    }else{
+        console.log('connected successfully');
+        sendData();
+        connection.end();
+    }    
+});
+
+function sendData() {
+   
+  //sends the patient information to the database
+route.post('/submit_patient', function (req, res) {
+    var sql = "INSERT INTO patient (firstname,lastname,sirname,dob,email,phone,residence,gender) VALUES('" + req.body.firstname + "','" + req.body.lastname + "','" + req.body.sirname + "','" + req.body.date + "','" + req.body.email + "','" + req.body.phone + "','" + req.body.residence + "','" + req.body.gender + "')";
+    connection.query(sql, function (err) {
+        if (err) {
+            throw err;
+        } else {
+            console.log('data inserted successfully');
+            res.end("inserted successfully")
+        }
+    })
+    
+});
+
+//sends the dentist information to the server
+route.post('/submit_dentist', function (req, res) {
+    var sql = "INSERT INTO dentist (firstname,lastname,sirname,dob,email,phone,residence,account,gender,national_id) VALUES('" + req.body.firstname + "','" + req.body.lastname + "','" + req.body.sirname + "','" + req.body.dob + "','" + req.body.email + "','" + req.body.phone + "','" + req.body.residence + "','" + req.body.account + "','" + req.body.gender + "','" + req.body.national + "');";
+    connection.query(sql, function (err) {
+        if (err) {
+            throw err;
+        } else {
+            console.log('data inserted successfully');
+            res.end("inserted successfully");
+        }
+    })
+
+});
+
+//sends the assistants information to the server
+route.post('/submit_assistant', function (req, res) {
+    var sql = "INSERT INTO dentist (firstname,lastname,sirname,dob,email,phone,residence,account,gender,national_id) VALUES('" + req.body.firstname + "','" + req.body.lastname + "','" + req.body.sirname + "','" + req.body.dob + "','" + req.body.email + "','" + req.body.phone + "','" + req.body.residence + "','" + req.body.account + "','" + req.body.gender + "','" + req.body.national + "');";
+    connection.query(sql, function (err) {
+        if (err) {
+            throw err;
+        } else {
+            console.log('data inserted successfully');
+            res.end("inserted successfully");
+        }
+    })
+
+});
+   
+}
 
 route.listen(4000);
